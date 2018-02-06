53e53b3710a2383b6f1cdc53c25867628394af87
RagnarLodbrok2017 <35165213+RagnarLodbrok2017@users.noreply.github.com>
2018-02-06 23:17:59 +0200

    Making style

---
+++ front/src/components/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+  <nav class="navbar navbar-inverse">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">Galaxy Movies</a>
+      </div>
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse">
+        <ul class="nav navbar-nav navbar-right">
+          <li><a class="scroll-link" href="#">Features</a></li>
+          <li><a class="scroll-link" href="#">How it works</a></li>
+          <li><a class="scroll-link" href="#">Testimonials</a></li>
+          <li><a class="scroll-link" href="#">FAQ</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ front/src/components/authentication/Register.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="top-content" style="position: relative; z-index: 0; background: none;">
+    <div class="container">
+
+      <div class="row">
+        <div class="col-sm-8 col-sm-offset-2 text">
+          <h1 class="wow fadeInLeftBig animated" style="visibility: visible; animation-name: fadeInLeftBig;">Register Page</h1>
+          <div class="description wow fadeInLeftBig animated" style="visibility: visible; animation-name: fadeInLeftBig;">
+            <p>
+              We have been working very hard to create the new version of our app. It comes with a lot of new features. Check it out now!
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-sm-6 col-sm-offset-3 r-form-1-box wow fadeInUp animated" style="visibility: visible; animation-name: fadeInUp;">
+          <div class="r-form-1-top">
+            <div class="r-form-1-top-left">
+              <h3>Sign up now</h3>
+              <p>Fill in the form below to get instant access:</p>
+            </div>
+            <div class="r-form-1-top-right">
+              <i class="fa fa-pencil"></i>
+            </div>
+          </div>
+          <div class="r-form-1-bottom">
+            <form role="form" action="" method="post">
+              <div class="form-group">
+                <label class="sr-only" for="r-form-1-first-name">name</label>
+                <input type="text" name="r-form-1-name" placeholder="name..." class="r-form-1-first-name form-control" id="r-form-1-first-name">
+              </div>
+              <div class="form-group">
+                <label class="sr-only" for="r-form-1-email">Email</label>
+                <input type="email" name="r-form-1-email" placeholder="Email..." class="r-form-1-email form-control" id="r-form-1-email">
+              </div>
+              <div class="form-group">
+                <label class="sr-only" for="r-form-1-last-name">Username</label>
+                <input type="text" name="r-form-1-username" placeholder="Username..." class="r-form-1-last-name form-control" id="r-form-1-last-name">
+              </div>
+              <div class="form-group">
+                <label class="sr-only" for="r-form-1-last-name">Password</label>
+                <input type="password" name="r-form-1-password" placeholder="Password..." class="r-form-1-last-name form-control" id="r-form-1-last-name">
+              </div>
+              <div class="form-group">
+                <label class="sr-only" for="r-form-1-about-yourself">About yourself</label>
+                <textarea name="r-form-1-about-yourself" placeholder="About yourself..." class="r-form-1-about-yourself form-control" id="r-form-1-about-yourself"></textarea>
+              </div>
+              <button type="submit" class="btn">Sign me up!</button>
+            </form>
+          </div>
+        </div>
+      </div>
+
+    </div>
+    <div class="backstretch" style="left: 0px; top: 0px; overflow: hidden; margin: 0px; padding: 0px; height: 899px; width: 1349px; z-index: -999998; position: absolute;"><img src="../../assets/images/backgrounds/1.jpg" style="position: absolute; margin: 0px; padding: 0px; border: none; width: 1349px; height: 899.333px; max-height: none; max-width: none; z-index: -999999; left: 0px; top: -0.166667px;"></div></div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style>
+
+</style>
